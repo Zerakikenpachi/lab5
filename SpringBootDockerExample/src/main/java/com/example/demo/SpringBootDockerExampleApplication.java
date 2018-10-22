fb3d5e7be264bd9e048ce8cb8b1ddf24e887fa3d
Unknown <victora.agudelo@udea.edu.co>
2018-10-21 23:21:51 -0500

    Final

--- SpringBootDockerExample/src/main/java/com/example/demo/SpringBootDockerExampleApplication.java
+++ SpringBootDockerExample/src/main/java/com/example/demo/SpringBootDockerExampleApplication.java
@@ -5,16 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @SpringBootApplication
 @RestController
 public class SpringBootDockerExampleApplication {
-
 	@RequestMapping("/")
 	public String home() {
 	return "Hello Docker World";
 	}
 	public static void main(String[] args) {
-	SpringApplication.run(SpringBootDockerExampleApplication.class,	args);
+	SpringApplication.run(SpringBootDockerExampleApplication.class,
+	args);
 	}
 }
